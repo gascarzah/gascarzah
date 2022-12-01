ca6fee1e94a1ade27a2ec892d82e80d97e89084f
Giovanni Ascarza Hinostroza <gascarzah@gmail.com>
2022-12-01 18:55:05 -0500

    inicial

---
+++ components/Layout.js
@@ -0,0 +1,18 @@
+import React, { Children } from 'react'
+import Footer from './Footer'
+import Header from './Header'
+import Navbar from './Navbar'
+
+const Layout = ({ children }) => {
+    return (
+        <>
+            <Header />
+            <Navbar />
+
+            {children}
+            <Footer />
+        </>
+    )
+}
+
+export default Layout
